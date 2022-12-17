c5c9358b3459987661c5115c55824227c8c64376
bet00n <luka9pl@gmail.com>
2022-12-17 16:14:48 +0100

    answers handler feature

--- movies.js
+++ movies.js
@@ -1,13 +1,17 @@
 const movies = [
     {title: 'Iron Man',
     image: "images/iron-man.jpg",
-    bonusQuestions: ['q1','q2','q3']},
+    hints: ['q1','q2','q3']},
     {title: 'Iron Man 2',
     image: "images/iron-man-2.jpg",
-    bonusQuestions: ['q1','q2','q3']},
+    hints: ['q1','q2','q3']},
     {title: 'Avengers',
     image: "images/avengers.jpg",
-    bonusQuestions: ['q1','q2','q3']},
+    hints: ['q1','q2','q3']},
+    {title: "Iron Man 3",
+    image: "images/iron-man-3.jpg",
+    hints: ['q1','q2','q3']},
+    {title: "Ant Man",
+    image: "images/ant-man.jpg",
+    hints: ['q1','q2','q3']}
 ];
-
-console.log(movies);
--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 const root = document.querySelector(':root');
-const rootStyles = getComputedStyle(root);
-const topClipValue = rootStyles.getPropertyValue('--topClipValue');
-const rightClipValue = rootStyles.getPropertyValue('--rightClipValue');
-const bottomClipValue = rootStyles.getPropertyValue('--bottomClipValue');
-const leftClipValue = rootStyles.getPropertyValue('--leftClipValue');
 const splash = document.querySelector('.splash-element');
+const optionButtons = document.querySelectorAll('.answers button');
+const totalPointsDisplay = document.querySelector('.points span');
 
-const MAX_CLIP_PERCENTAGE = 80;
+let MAX_CLIP_PERCENTAGE = 80;
+let totalPoints = 0;
 
 const generateNumber = (max) => {
     const num = Math.floor(Math.random()*max);  
     return num;  
 }
 
-
-const generateSplash = () => {
+const generateMovieIndex = () => {
     const idx = generateNumber(movies.length);
-    console.log(idx);
+    return idx;
+}
+
+const generateSplash = (idx) => {
     const randomTopClipValue = generateNumber(MAX_CLIP_PERCENTAGE);
     const randomRightClipValue = generateNumber(MAX_CLIP_PERCENTAGE);
     const randomBottomClipValue = (MAX_CLIP_PERCENTAGE - randomTopClipValue);
@@ -27,4 +27,53 @@ const generateSplash = () => {
     root.style.setProperty('--bottomClipValue', `${randomBottomClipValue}` + '%');
     root.style.setProperty('--leftClipValue', `${randomLeftClipValue}` + '%');
 }
-generateSplash();
+
+const generateAnswers = (validAnswer) => {
+    const optionsPool = [validAnswer];
+    const tempMovieArr = [...movies];
+    tempMovieArr.sort(() => 0.5 - Math.random());
+    for (i = 0 ; i < 3 ; i++) {
+        optionsPool.push(tempMovieArr.shift());
+    }
+    optionsPool.sort(() => 0.5 - Math.random());
+    optionButtons.forEach((btn) => {
+        btn.innerHTML = optionsPool.shift().title;
+    })
+}
+
+const validateAnswer = (validAnswer) => {
+    const validTitle = validAnswer.title;
+    console.log(validTitle);
+    optionButtons.forEach((btn) => {
+        btn.addEventListener('click',(validTitle) => {
+            if (btn.innerHTML == validAnswer.title) {
+                console.log('good answer');
+                //add bonus points
+                setPoints(10);
+            } else {
+                console.log('wrong answer');
+            }
+            //show image
+            playRound();
+        })
+    })
+}
+
+const setPoints = (points) => {
+    totalPoints += points;
+    totalPointsDisplay.innerHTML = totalPoints;
+}
+
+const playRound = () => {
+    const idx = generateMovieIndex();
+    const validAnswer = {title: movies[idx].title,
+                         image: movies[idx].image,
+                         hints: movies[idx].hints};
+    console.log(validAnswer);
+    generateSplash(idx);
+    movies.splice(idx,1);
+    generateAnswers(validAnswer);
+    validateAnswer(validAnswer);
+}
+
+playRound();
